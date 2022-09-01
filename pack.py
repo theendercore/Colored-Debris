0c477b15a5e9ed28d39975380bc6e418b9ec4904
theendercore <theendercore@gmail.com>
2022-09-01 21:45:03 +0300

    added animated textures

--- pack.py
+++ pack.py
@@ -1,10 +1,21 @@
+from zipfile import ZipFile
 import os
-path = "./colors"
+
+pack_version = "1.4.0"
+mcmeta = "./pack.mcmeta"
 pack_name = " Ancient Debris"
-colors = []
-
-for color in os.listdir(path):
-    colors.append(color)
+file_path = "./assets/minecraft/textures/block/ancient_debris_"
+colors_amount = 0
 
 
-print(colors)
+for color in os.listdir("./colors"):
+    if color.endswith("Rainbow"):
+
+    with ZipFile('./expo/'+color+pack_name+" "+pack_version+'.zip', 'w') as resourcepack:
+        resourcepack.write(mcmeta)
+        resourcepack.write("./colors/"+color+"/top.png", file_path+"top.png")
+        resourcepack.write("./colors/"+color+"/side.png", file_path+"side.png")
+        resourcepack.write("./colors/"+color+"/side.png", "./pack.png")
+    colors_amount += 1
+
+print(colors_amount)
